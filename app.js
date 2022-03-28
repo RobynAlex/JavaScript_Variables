7246c0a28efb3bfc8a345f6813cdd0e6923b237d
Robyn Alex <robyn.alexander1111@gmail.com>
2022-03-28 11:40:55 -0500

    updated commit

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Robyn";
+
+let lastName = "Alex1"
+
+var age = 22;
 
 // Exercise 2
+let fullName = firstName + " " + lastName
+ console.log(fullName);
 
 // Exercise 3
+let myStory;
+
+let home = "Far Far Away";
+
+let pastime = "basketball";
+
+myStory = `Hello! My name is ${fullName}. I live in ${home}. I enjoy ${pastime} and coding.`;
+ console.log(myStory);
+
+
